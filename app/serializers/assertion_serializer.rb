e523b9fd35224a4d277fb08ef10c54b29df35a8c
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2020-07-07 20:30:34 -0600

    adding id to the assertion serializer

--- app/serializers/assertion_serializer.rb
+++ app/serializers/assertion_serializer.rb
@@ -1,3 +1,3 @@
 class AssertionSerializer < ActiveModel::Serializer
-  attributes :assertion_name, :assertion_result, :request_id
+  attributes :id, :assertion_name, :assertion_result, :request_id
 end
